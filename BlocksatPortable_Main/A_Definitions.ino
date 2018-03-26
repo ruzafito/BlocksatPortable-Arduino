faf069f8f54f7a43d108729104d1b6c9e3c538ee
Sergio <sergio.ibiza92@gmail.com>
2018-03-26 20:22:11 +0200

    First commit

---
+++ BlocksatPortable_Main/A_Definitions.ino
@@ -0,0 +1,16 @@
+/**
+   ----------------------------
+   TFM - Blocksat Portable v1.0
+   ----------------------------
+
+   Sergio del Horno Torres
+   ----------------------------
+   A_Definitions
+   In this tab are defined the constants used in other tabs.
+   Definition of PINs, TIMEOUTs & Global Variables
+   ----------------------------
+   2018
+*/
+
+// Baud Rate
+#define baudRate        115200
---
+++ BlocksatPortable_Main/BlocksatPortable_Main.ino
@@ -0,0 +1,25 @@
+/**
+   ----------------------------
+   TFM - Blocksat Portable v1.0
+   ----------------------------
+
+   Sergio del Horno Torres
+   ----------------------------
+   BlocksatPortable_Main
+   This is the Main tab.
+   Contains setup() and loop() methods.
+   This tab just call init methods in setup() and
+   motor methods in loop().
+   ----------------------------
+   2018
+*/
+
+void setup() {
+  initSystem();
+
+}
+
+void loop() {
+  
+
+}
